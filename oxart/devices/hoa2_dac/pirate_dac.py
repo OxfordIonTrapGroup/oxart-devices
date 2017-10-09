cbbfbd14b7a3b4f83485361dfaaaaef919fa823c
ion <ion@HOA2MSTR.nat.physics.ox.ac.uk>
2017-10-09 12:10:01 +0100

    devices: wrote driver and wrapper for HOA2 test DAC

---
+++ oxart/devices/hoa2_dac/pirate_dac.py
@@ -0,0 +1,74 @@
+# Require pyBusPirateLite
+# https://github.com/juhasch/pyBusPirateLite.git
+
+import time
+from pyBusPirateLite.SPI import *
+
+# CS : SYNC_N
+# AUX : LDAC
+class PirateDac:
+    def __init__(self, port):
+        self.spi = SPI(portname=port)
+        self.spi.pins = PIN_POWER | PIN_AUX
+        self.spi.config = CFG_PUSH_PULL
+        self.spi.speed = '1MHz'
+        self.spi.cs = False
+
+        # Config +-10V range, power up
+        self._write(0x1f, reg=2, ch=0)
+        self._write(0x4, reg=1, ch=4)
+
+
+    def pulse_ldac(self):
+        self.spi.pins = PIN_POWER | PIN_CS 
+        time.sleep(0.1e-3)
+        self.spi.pins = PIN_POWER | PIN_CS | PIN_AUX
+
+    def _write(self, data, reg=0, ch=0, rw=0):
+        """Read/write a 16 bit data word to a given register and channel
+        reg : 0 -> DAC, 1 -> output range, 2 -> power control, 3 -> control
+        rw : 0 -> write, 1 -> read
+        ch : 0-3 for ch 0-3, 4 -> all
+        """
+        ctrl = (rw << 7) + ((reg & 0x7)<<3) + (ch & 0x7)
+        self.spi.write_then_read(3, 0, [ctrl, (data>>8)&0xff, data&0xff])
+
+    def _read(self):
+        self.spi.cs = True
+        return self.spi.transfer([0x18, 0x0, 0x0])
+        self.spi.cs = False
+
+    def set_channel(self, v, ch=0, update=True):
+        """Set channel to a given voltage.
+        If update, pulse LDAC and update immediately,
+        else batch and update together on LDAC"""
+
+        if v > 10 or v < -10:
+            raise ValueError("Voltage out of range")
+
+        raw = int(v / 10 * 32767)
+        if raw < 0:
+            raw += 2**16
+        self._write(raw, ch=ch)
+
+        if update:
+            self.pulse_ldac()
+
+
+
+if __name__ == "__main__":
+    print("Opening ...")
+    dac = PirateDac('com14')
+
+    print("Sweeping DACs")
+    def write(v):
+        for ch in range(4):
+            dac.set_channel(v, ch=ch, update=False)
+        dac.pulse_ldac()  
+
+    i=0
+    while True:
+        i += 1
+        if i > 10:
+            i = -10
+        write(i)
