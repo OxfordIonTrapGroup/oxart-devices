4d2158cad4b744de1e6fbeb56f569bba56db15a2
Thomas Harty <thomas.harty@physics.ox.ac.uk>
2018-07-09 16:32:11 +0100

    lakeshore 335: fix ping method in driver

--- oxart/devices/lakeshore_335/driver.py
+++ oxart/devices/lakeshore_335/driver.py
@@ -1,23 +1,22 @@
 """ Driver for Lake Shore Cryogenics Model 335 Temperature controllers """
 
-from oxart.devices.streams import Ethernet
-
 
 class LakeShore335:
 
-    def __init__(self, addr, port=1234):
-        self.sock = Ethernet(addr, port)
+    def __init__(self, stream):
+        self.stream = stream
+        assert self.ping()
 
     def identify(self):
-        self.sock.write("*IDN?\n".encode())
-        return self.sock.readline().decode()
+        self.stream.write("*IDN?\n".encode())
+        return self.stream.readline().decode()
 
     def get_temp(self, input="A"):
         """ Returns the temperature of an input channel as a float in Kelin
         : param input: either "A" or "B"
         """
-        self.sock.write("KRDG? {}\n".format(input).encode())
-        return float(self.sock.readline())
+        self.stream.write("KRDG? {}\n".format(input).encode())
+        return float(self.stream.readline())
 
     def ping(self):
         idn = self.identify().split(',')
