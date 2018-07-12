2de274e5d6f45f3072034d94dcdd752eb7f3e14f
Thomas Harty <thomas.harty@physics.ox.ac.uk>
2018-07-12 14:23:18 +0100

    streams: add address_args and get_stream functions to allow controllers to be agnostic about the physical connection to the device

--- oxart/devices/streams.py
+++ oxart/devices/streams.py
@@ -3,9 +3,62 @@ and Ethernet, allowing drivers to be agnostic about the physical connection to
 the device. """
 
 import socket
+import serial
+import ipaddress
+from oxart.devices.prologix_gpib.driver import GPIB
 
 
-class Ethernet():
+def address_args(parser, gpib=None):
+    """ Add command-line arguments related to device addressing.
+
+    :param gpib: if not None, the device supports a gpib interface. If
+    isinstance(gpib, int) we use this as the default gpib address. NB devices
+    with a GPIB address are assumed to be GPIB devices by get_stream.
+
+    Note that we do not currently allow a "-p", "--port" argument for Ethernet
+    devices, which would conflict with the port argument used by
+    simple_network_server. In general, this doesn't seem to be that useful
+    anyway.
+
+    Use get_stream to return a connection to the device.
+    """
+    if gpib is not None:
+        parser.add_argument("-d", "--device",
+                            help="GPIB controller address")
+        parser.add_argument("-gpib", "--gpib_addr",
+                            help="device's GPIB address",
+                            default=gpib if isinstance(gpib, int) else None)
+    else:
+        parser.add_argument("-d", "--device", help="device's address")
+
+
+def get_stream(args, baudrate=115200, port=None):
+    """ Returns a connection the device whose address is given in args.
+
+    If args has a valid gpib address, we assume the device is a GPIB device.
+    Note that we currently only support one device per GPIB controller.
+
+    :param baudrate: baudrate to use for serial connections (default:115200)
+    :param port: port to use for Ethernet connections
+    """
+    if hasattr(args, "gpib_addr") and isinstance(args.gpib_addr, int):
+        port = 1234  # Port used by Prologix GPIB Ethernet controllers
+
+    dev = None
+    try:
+        ipaddress.ip_address(args.device)
+    except ValueError:
+        if args.device.startswith("/dev/") \
+           or args.device.lower().startswith("com"):
+            dev = serial.Serial(args.device, baudrate=baudrate)
+    else:
+        dev = Ethernet(args.device, port)
+    if hasattr(args, "gpib_addr") and isinstance(args.gpib_addr, int):
+        dev = GPIB(dev).get_stream(args.gpib_addr)
+    return dev
+
+
+class Ethernet:
     """ Simple pySerial-compatible wrapper for Ethernet sockets. """
     def __init__(self, addr, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
