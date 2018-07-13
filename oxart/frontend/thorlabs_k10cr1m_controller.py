4ba8fb0e796b8918451293172dbae1dab2ec804a
Lab one ALWSDAQ93 <steaneGuest@physics.ox.ac.uk>
2018-07-13 16:13:49 +0100

    changed port number

--- oxart/frontend/thorlabs_k10cr1m_controller.py
+++ oxart/frontend/thorlabs_k10cr1m_controller.py
@@ -9,7 +9,7 @@ from artiq.tools import verbosity_args, simple_network_args, init_logger
 
 def get_argparser():
     parser = argparse.ArgumentParser(description="ARTIQ controller for the Thorlabs K10CR1/M motorised rotation mount")
-    simple_network_args(parser, 4001)
+    simple_network_args(parser, 4002)
     parser.add_argument("-s", "--serial", default=None,
                         help="serial number of device. Uses first device if not provided")
     verbosity_args(parser)
