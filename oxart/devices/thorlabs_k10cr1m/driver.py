78e2fade63f833f39d7d1c942a16156246904135
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-07-16 12:14:47 +0100

    fix potential bug in waveplate controller and add some logging

--- oxart/devices/thorlabs_k10cr1m/driver.py
+++ oxart/devices/thorlabs_k10cr1m/driver.py
@@ -1,52 +1,48 @@
 import logging
-import serial
-import re
-import sys
-import asyncio
 import thorlabs_apt as apt
 
-import artiq.protocols.pyon as pyon
+K10CR1_TYPE = 50
 
 logger = logging.getLogger(__name__)
 class K10CR1MDriver:
-    def __init__(self, serial=None):
+    def __init__(self, sn=None):
         dev_list = apt.list_available_devices()
 
         if len(dev_list) == 0:
             raise Exception("No APT devices found")
 
-
-        if [dev[0] is 50 for dev in dev_list]:
-            dev_sn_available = [dev[1] for dev in dev_list]
+        dev_sn_available = [dev[1] for dev in dev_list if dev[0] == K10CR1_TYPE]
+        logger.info("Found serial nos. {}".format(dev_sn_available))
 
         if len(dev_sn_available) == 0:
             raise Exception("No K10CR1 devices found")
 
-
-        if serial is None:
-            serial = dev_sn_available[0]
+        if sn is None:
+            sn = dev_sn_available[0]
 
         # Check serial number is present in available devices
+        if sn not in dev_sn_available:
+            raise ValueError("No device with serial no. {} present (available = {})"\
+                .format(sn, dev_sn_available))
 
-        try:
-            dev_sn_available.index(serial)
-        except ValueError:
-            raise ValueError("No device with serial number {} present (available = {})"\
-                .format(serial, dev_sn_available))
-
-        self.dev = apt.Motor(serial)
+        logger.debug("Initialising connection to motor serial no. {}".format(sn))
+        self.dev = apt.Motor(sn)
 
         #home the motor - a bug in the software is that home parameters must be set
         #have set them to the 0 position on the vernier
+        logger.debug("Homing motor serial no. {}".format(sn))
         self.dev.set_move_home_parameters(2,1,1,4)
         self.dev.move_home(True)
 
+        logger.debug("Setting velocity parameters for motor serial no. {}".format(sn))
         self.dev.set_velocity_parameters(0,4,6)
 
     def set_angle(self, angle):
+        """Set angle in degrees"""
         self.dev.move_to(angle, blocking=True)
 
     def get_angle(self):
+        """Get angle in degrees"""
         return self.dev.position
 
     def ping(self):
