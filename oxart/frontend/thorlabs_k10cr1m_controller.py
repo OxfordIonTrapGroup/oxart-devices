ca637175d8609337ace53684b8b22f5ceacb544c
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-07-31 12:12:36 +0100

    make thorlabs rotation mount controller parse serial number properly

--- oxart/frontend/thorlabs_k10cr1m_controller.py
+++ oxart/frontend/thorlabs_k10cr1m_controller.py
@@ -10,7 +10,7 @@ from artiq.tools import verbosity_args, simple_network_args, init_logger
 def get_argparser():
     parser = argparse.ArgumentParser(description="ARTIQ controller for the Thorlabs K10CR1/M motorised rotation mount")
     simple_network_args(parser, 4002)
-    parser.add_argument("-s", "--serial-number", default=None,
+    parser.add_argument("-s", "--serial-number", default=None, type=int,
                         help="serial number of device. Uses first device if not provided")
     verbosity_args(parser)
     return parser
