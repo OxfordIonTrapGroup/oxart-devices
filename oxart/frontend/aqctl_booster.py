98f702bd67df8a569452dc7e95245280c23e003b
tph <thomas.harty@physics.ox.ac.uk>
2018-09-13 15:11:21 +0100

    booster: add controller

---
+++ oxart/frontend/aqctl_booster.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+
+from oxart.devices.booster.driver import Booster
+from artiq.protocols.pc_rpc import simple_server_loop
+from artiq.tools import verbosity_args, simple_network_args, init_logger
+
+logger = logging.getLogger(__name__)
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="ARTIQ controller for Booster"
+                                     " 8-channel RF power amplifier")
+    parser.add_argument("-d", "--device", help="device's IP address")
+    simple_network_args(parser, 4300)
+    verbosity_args(parser)
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+    init_logger(args)
+
+    dev = Booster(args.device)
+
+    try:
+        simple_server_loop({"Booster": dev}, args.bind, args.port)
+    finally:
+        dev.close()
+
+
+if __name__ == "__main__":
+    main()
