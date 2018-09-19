84231d88a36155e368e2150203deb7c0ee8339ba
tph <thomas.harty@physics.ox.ac.uk>
2018-09-19 16:40:20 +0100

    add scpi_dmm controller

---
+++ oxart/frontend/aqctl_scpi_dmm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+
+from oxart.devices.streams import address_args, get_stream
+from oxart.devices.scpi_dmm.driver import ScpiDmm
+from artiq.protocols.pc_rpc import simple_server_loop
+from artiq.tools import verbosity_args, simple_network_args, init_logger
+
+logger = logging.getLogger(__name__)
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="ARTIQ controller for "
+                                     "SCPI Digital Multi Meters")
+    address_args(parser, gpib=5)
+    simple_network_args(parser, 4300)
+    verbosity_args(parser)
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+    init_logger(args)
+
+    dev = ScpiDmm(get_stream(args))
+
+    try:
+        simple_server_loop({"ScpiDmm": dev}, args.bind, args.port)
+    finally:
+        dev.close()
+
+
+if __name__ == "__main__":
+    main()
