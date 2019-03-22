0ac04793369d395072269fe3d19cd55cc6aedfa0
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2019-03-22 13:23:39 +0000

    devices/booster: refactor interface (breaking change)
    The Booster SCPI interface has now been finalised and tested
    against this interface.
    There is one breaking change in this refactor:
    get_interlock_status() has been replaced by get_interlock_tripped()
    for clarity (get_interlock_status() returned True if everything was
    OK)

--- oxart/devices/booster/driver.py
+++ oxart/devices/booster/driver.py
@@ -12,9 +12,9 @@ def _validate_channel(channel):
 class Booster:
     """ Booster 8-channel RF PA """
     def __init__(self, device):
-
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((device, 5000))
+        self.sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
         self.ip_addr = device
         assert self.ping()
 
@@ -39,22 +39,20 @@ class Booster:
         idn = self.identify().lower().split(',')
         return idn[0:2] == ["wut", "rfpa booster"]
 
-    def _cmd(self, channel, cmd, arg=""):
+    def _cmd(self, cmd, channel, arg=None):
         _validate_channel(channel)
-        if arg != "":
+        if arg is not None:
             full_cmd = "{} {},{}".format(cmd, channel, arg)
         else:
             full_cmd = "{} {}".format(cmd, channel)
-        self.sock.send((full_cmd+'\r\n').encode())
+        self.sock.send((full_cmd+'\n').encode())
 
-        # deleteme!
         if '?' not in cmd:
-            import time
-            time.sleep(0.3)
             return
 
         with self.sock.makefile() as stream:
             response = stream.readline().lower().strip()
+            # print("RESPONSE: "+response)
 
             if ('?' not in cmd and response != "okay") or \
                ('?' in cmd and 'error' in response):
@@ -62,8 +60,8 @@ class Booster:
                     full_cmd, response))
             return response
 
-    def _query_bool(self, channel, cmd, arg=""):
-        resp = self._cmd(channel, cmd, arg)
+    def _query_bool(self, cmd, channel, arg=None):
+        resp = self._cmd(cmd, channel, arg)
         if resp == "0":
             return False
         elif resp == "1":
@@ -74,7 +72,15 @@ class Booster:
 
     def set_enabled(self, channel, enabled=True):
         """ Enables/disables a channel. """
-        self._cmd(channel, "CHAN:ENABLE", str(int(enabled)))
+        if enabled:
+            cmd = "CHAN:ENAB"
+        else:
+            cmd = "CHAN:DISAB"
+        self._cmd(cmd, channel)
+
+    def get_enabled(self, channel):
+        """ Query if a channel is enabled """
+        return bool(self._cmd("CHAN:ENAB?", channel))
 
     def set_output_interlock(self, channel, threshold):
         """ Sets the output forward power interlock threshold for a channel in
@@ -88,57 +94,44 @@ class Booster:
         if (threshold < 0) or (threshold > 38):
             raise ValueError("Output forward power interlock threshold must "
                              "lie between 0dBm and +38dBm")
-        self._cmd(channel, "INT:POW", "{:.2f}".format(threshold))
+        self._cmd("INT:POW", channel, "{:.2f}".format(threshold))
 
     def get_output_interlock(self, channel):
         """
         Returns the output forward power interlock threshold for a channel in
         dBm.
         """
-        return float(self._cmd(channel, "INT:POW?"))
+        return float(self._cmd("INT:POW?", channel))
 
-    def get_interlock_status(self, channel):
+    def get_interlock_tripped(self, channel):
         """
-        Returns True if all interlocks for a channel are okay, False if an
-        interlock has tripped.
+        Returns True if any of the interlocks on a channel have tripped, or
+        False if the channel is operating normally
         """
-        return not self._query_bool(channel, "INT:STAT?")
+        return self._query_bool("INT:STAT?", channel)
 
     def get_overload_status(self, channel):
         """
         Returns True if the power interlock for a channel is okay, False if
         it has tripped.
         """
-        return self._query_bool(channel, "INT:OVER?")
+        return self._query_bool("INT:OVER?", channel)
 
     def get_temperature(self, channel):
         """ Returns the temperature of a channel in C """
-        return float(self._cmd(channel, "MEAS:TEMP?"))
+        return float(self._cmd("MEAS:TEMP?", channel))
 
     def clear_interlock(self, channel):
         """ Resets all interlocks for a channel. """
-        self._cmd(channel, "INT:CLEAR")
+        self._cmd("INT:CLEAR", channel)
 
     def get_output_power(self, channel):
         """ Returns the output power for a channel in dBm """
-        return float(self._cmd(channel, "MEAS:OUT?"))
+        return float(self._cmd("MEAS:OUT?", channel))
 
     def get_reverse_power(self, channel):
         """ Returns the output reflected power for a channel in dBm """
-        return float(self._cmd(channel, "MEAS:REV?"))
+        return float(self._cmd("MEAS:REV?", channel))
 
     def close(self):
         self.sock.close()
-
-
-def main():
-    pa = Booster("10.255.6.216")
-    [pa.set_output_interlock(ch, 30) for ch in range(8)]
-    for channel in range(7):
-        print("{}: {}".format(channel, pa.get_interlock_status(channel)))
-    # to do: trip temperature interlock and check difference between interlock
-    # status and overload status
-
-
-if __name__ == "__main__":
-    main()
