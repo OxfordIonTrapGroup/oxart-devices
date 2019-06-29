63c3f4859abd5657357857272080aed78f248481
pdrmota <peter.drmota@physics.ox.ac.uk>
2019-06-29 09:49:04 +0100

    create controller for DDR05 analogously to DDR25

---
+++ oxart/frontend/thorlabs_ddr05_controller.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.5
+
+import argparse
+import sys
+
+from oxart.devices.thorlabs_apt.driver import DDR05
+from artiq.protocols.pc_rpc import simple_server_loop
+from artiq.tools import (simple_network_args, init_logger,
+                         bind_address_from_args)
+from oxart.tools import add_common_args
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="ARTIQ controller for the "
+                                    "Thorlabs DDR05 motorised rotation mount")
+    simple_network_args(parser, 4000)
+    parser.add_argument("-d", "--device", default=None,
+                        help="serial device. See documentation for how to "
+                             "specify a USB Serial Number.")
+    parser.add_argument("--no-auto-home", action="store_true",
+                        help="Do not home (reset to mechanical zero) on \
+                        start (this needs to be done each time the hardware \
+                        is power cycled")
+    add_common_args(parser)
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+    init_logger(args)
+
+    dev = DDR05(args.device, auto_home = not args.no_auto_home)
+
+    simple_server_loop({"ddr05": dev}, bind_address_from_args(args), args.port)
+
+
+if __name__ == "__main__":
+    main()
