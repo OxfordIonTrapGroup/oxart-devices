7b51fa29daf0598c7fa8084b800290776af76a1c
hartytp <thomas.harty@gmail.com>
2019-07-30 17:35:24 +0100

    booster.driver: make the damn thing work [wip]

--- oxart/devices/booster/driver.py
+++ oxart/devices/booster/driver.py
@@ -1,65 +1,64 @@
-import socket
-import numpy as np
-
-
-def _validate_channel(channel):
-    if not isinstance(channel, int) and not isinstance(channel, np.int32):
-        raise ValueError("channel must be an int")
-    if (channel < 0) or (channel > 7):
-        raise ValueError("invalid channel number {}".format(channel))
+import serial
 
 
 class Booster:
     """ Booster 8-channel RF PA """
     def __init__(self, device):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((device, 5000))
-        self.sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
-        self.ip_addr = device
+        self.dev = serial.serial_for_url("socket://{}:5000".format(device))
+        self.dev.flushInput()
         assert self.ping()
-
 
     def identify(self):
         """ Returns a device identification string """
-        self.sock.send("*IDN?\r\n".encode())
-        with self.sock.makefile() as stream:
-            return stream.readline().strip()
+        self.dev.write(b"*IDN?\n")
+        idn = self.dev.readline().decode().strip()
+        self.dev.readline()
+        return idn
 
     def get_version(self):
-        """
-        Returns tuple of the revision number as a float, and the build date as
+        """ Returns tuple of the revision number as a float, and the build date as
         a string
         """
-        idn = self.identify().lower().split(',')
-        hw_rev = float(idn[2])
-        fw_build = idn[3].strip()
-        return hw_rev, fw_build
+        # to do
+        # idn = self.identify().lower().split(',')
+        # hw_rev = float(idn[2])
+        # fw_build = idn[3].strip()
+        # return hw_rev, fw_build
+        return None
 
     def ping(self):
         """ Returns True if we are connected to a Booster """
-        idn = self.identify().lower().split(',')
-        return (idn[0:2] == ["wut", "rfpa booster"] or idn[0][0:4] == "rfpa")
+        idn = self.identify().lower().split(' ')
+        return idn[0] == "rfpa"
 
     def _cmd(self, cmd, channel, arg=None):
-        _validate_channel(channel)
-        if arg is not None:
-            full_cmd = "{} {},{}".format(cmd, channel, arg)
-        else:
-            full_cmd = "{} {}".format(cmd, channel)
-        self.sock.send((full_cmd+'\n').encode())
+        if self.dev.in_waiting:
+            # self.dev.flushInput()  # to do: remove when fw doesn't suck
+            print(self.dev.readline())
+            raise ValueError("CRAP IN BUFFER, OH DEAR!!")
 
-        if '?' not in cmd:
+        # if channel not in range(8):
+        #     raise ValueError("invalid channel number {}".format(channel))
+
+        if arg is None:
+            cmd = "{} {}\n".format(cmd, channel)
+        else:
+            cmd = "{} {}, {}\n".format(cmd, channel, arg)
+
+        print("CMD: " + cmd + "...")
+        self.dev.write(cmd.encode())
+
+        if '?' not in cmd:  # to do: remove when fw does not suck
             return
 
-        with self.sock.makefile() as stream:
-            response = stream.readline().lower().strip()
-            # print("RESPONSE: "+response)
+        response = self.dev.readline().decode().lower().strip()
+        print("resp: " + response)
 
-            if ('?' not in cmd and response != "okay") or \
-               ('?' in cmd and 'error' in response):
-                raise Exception("Unrecognised response to '{}': '{}'".format(
-                    full_cmd, response))
-            return response
+        # if ('?' not in cmd and response != "okay") or \
+        # ('?' in cmd and 'error' in response):
+        # raise Exception("Unrecognised response to '{}': '{}'".format(
+        # full_cmd, response))
+        return response
 
     def _query_bool(self, cmd, channel, arg=None):
         resp = self._cmd(cmd, channel, arg)
@@ -71,21 +70,53 @@ class Booster:
             raise Exception(
                 "Unrecognised response to {}: '{}'".format(cmd, resp))
 
+    def _query_float(self, cmd, channel, arg=None):
+        resp = self._cmd(cmd, channel, arg)
+        try:
+            return float(resp)
+        except ValueError:
+            raise Exception(
+                "Unrecognised response to {}: '{}'".format(cmd, resp))
+
     def set_enabled(self, channel, enabled=True):
         """ Enables/disables a channel. """
-        if enabled:
-            cmd = "CHAN:ENAB"
-        else:
-            cmd = "CHAN:DISAB"
+        cmd = "CHAN:ENAB" if enabled else "CHAN:DISAB"
         self._cmd(cmd, channel)
 
     def get_enabled(self, channel):
-        """ Query if a channel is enabled """
-        return bool(self._cmd("CHAN:ENAB?", channel))
+        """ Query whether a channel is enabled """
+        return self._query_bool("CHAN:ENAB?", channel)
 
-    def set_output_interlock(self, channel, threshold):
-        """ Sets the output forward power interlock threshold for a channel in
-        dBm.
+    def get_detected(self, channel):
+        """ Returns True is the channel is detected, otherwise False.
+
+        Non-detected channels indicate a serious hardware error!
+        """
+        return self._query_bool("CHAN:DET?", channel)
+
+    def get_current(self, channel):
+        """ Returns the FET bias current (A) for a given channel. """
+        return self._query_float("MEAS:CURR?", channel)
+
+    def get_temperature(self, channel):
+        """ Returns the temperature (C) for a given channel. """
+        return self._query_float("MEAS:TEMP?", channel)
+
+    def get_power(self, channel):
+        """ Returns the output (forwards) power for a channel in dBm """
+        return self._query_float("MEAS:OUT?", channel)
+
+    def get_reflected(self, channel):
+        """ Returns the reflected power for a channel in dBm """
+        return self._query_float("MEAS:REV?", channel)
+
+    def get_fan(self):
+        """ Returns the fan speed as a number between 0 and 1"""
+        return self._query_float("MEAS:FAN?", "")
+
+    def set_interlock(self, channel, threshold):
+        """
+        Sets the output forward power interlock threshold (dBm) for a channel.
 
         This setting is stored in non-volatile memory and retained across power
         cycles.
@@ -97,42 +128,33 @@ class Booster:
                              "lie between 0dBm and +38dBm")
         self._cmd("INT:POW", channel, "{:.2f}".format(threshold))
 
-    def get_output_interlock(self, channel):
+    def get_interlock(self, channel):
+        """ Returns the output forward power interlock threshold (dBm) for a
+        channel.
         """
-        Returns the output forward power interlock threshold for a channel in
-        dBm.
-        """
-        return float(self._cmd("INT:POW?", channel))
-
-    def get_interlock_tripped(self, channel):
-        """
-        Returns True if any of the interlocks on a channel have tripped, or
-        False if the channel is operating normally
-        """
-        return self._query_bool("INT:STAT?", channel)
-
-    def get_overload_status(self, channel):
-        """
-        Returns True if the power interlock for a channel is okay, False if
-        it has tripped.
-        """
-        return self._query_bool("INT:OVER?", channel)
-
-    def get_temperature(self, channel):
-        """ Returns the temperature of a channel in C """
-        return float(self._cmd("MEAS:TEMP?", channel))
+        return self._query_float("INT:POW?", channel)
 
     def clear_interlock(self, channel):
-        """ Resets all interlocks for a channel. """
+        """ Resets the interlock for a given channel. """
         self._cmd("INT:CLEAR", channel)
 
-    def get_output_power(self, channel):
-        """ Returns the output power for a channel in dBm """
-        return float(self._cmd("MEAS:OUT?", channel))
+    # def get_interlock_status(self, channel):
+    #     """
+    #     Returns True if any of the interlocks on a channel have tripped, or
+    #     False if the channel is operating normally
+    #     """
+    #     return self._query_bool("INT:STAT?", channel)
 
-    def get_reverse_power(self, channel):
-        """ Returns the output reflected power for a channel in dBm """
-        return float(self._cmd("MEAS:REV?", channel))
+    # def get_overload_status(self, channel):
+    #     """
+    #     Returns True if the power interlock for a channel is okay, False if
+    #     it has tripped.
+    #     """
+    #     return self._query_bool("INT:OVER?", channel)
+
+    # def get_diagnostics(self, channel):
+    #     """ Channe """
+    #     return self._cmd("INT:DIAG?", channel)
 
     def close(self):
-        self.sock.close()
+        self.dev.close()
