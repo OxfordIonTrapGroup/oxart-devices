05f825a74034411c225ee638ac6d6d665e0f4c4f
Ryan Hanley <ryan.hanley@physics.ox.ac.uk>
2020-01-23 16:54:12 +0000

    frontend: Updated controllers to work with sipyco
    Changed a couple of function names to work with sipyco.

--- oxart/frontend/aqctl_lakeshore_335.py
+++ oxart/frontend/aqctl_lakeshore_335.py
@@ -5,8 +5,8 @@ import logging
 
 from oxart.devices.lakeshore_335.driver import LakeShore335
 from sipyco.pc_rpc import simple_server_loop
-from sipyco.common_args import simple_network_args, init_logger_from_args
 from oxart.tools import add_common_args
+import sipyco.common_args as sca
 
 logger = logging.getLogger(__name__)
 
@@ -15,21 +15,24 @@ def get_argparser():
     parser = argparse.ArgumentParser(description="ARTIQ controller for Lake "
                                      "Shore Cryogenics model 335 temperature"
                                      "controllers")
-    parser.add_argument("-d", "--device", help="device's hardware address")
+    parser.add_argument("-d", "--device",
+                        default="gpib://socket://10.255.6.189:1234-5",
+                        help="device's hardware address")
 
-    simple_network_args(parser, 4300)
-    add_common_args(parser)
+    sca.simple_network_args(parser, 4300)
+    sca.verbosity_args(parser)
     return parser
 
 
 def main():
     args = get_argparser().parse_args()
-    init_logger_from_args(args)
+    sca.init_logger_from_args(args)
 
     dev = LakeShore335(args.device)
 
     try:
-        simple_server_loop({"LakeShore335": dev}, args.bind, args.port)
+        simple_server_loop({"LakeShore335": dev},
+                           sca.bind_address_from_args(args), args.port)
     finally:
         dev.close()
 
