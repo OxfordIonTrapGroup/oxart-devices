8ca42138f877ad4fb40a8b82a47e0be3c1b53f8b
Ana <ana.sotirova@physics.ox.ac.uk>
2020-09-27 15:36:21 +0100

    Add simple driver for old Thorlabs MDT693A piezo controllers

---
+++ oxart/devices/thorlabs_mdt693a/driver.py
@@ -0,0 +1,48 @@
+import logging
+import serial
+import re
+import sys
+import asyncio
+
+import sipyco.pyon as pyon
+
+
+logger = logging.getLogger(__name__)
+
+
+class PiezoController:
+    """Driver for Thorlabs MDT693A 3-channel open-loop piezo controller.
+    NB The knobs on the piezo controller must be set to 0 voltage (i.e. turned
+    fully counter-clockwise) before using the driver to set the voltages.
+    """
+    def __init__(self, device):
+        self.dev = serial.serial_for_url("socket://{}:9001".format(device),
+                                            timeout=1)
+        assert self.ping()
+
+    def close(self):
+        """Close the serial port."""
+        self.dev.close()
+
+    def ping(self):
+        self._send_cmd("I")
+        return bool(self._read_line())
+
+    def _send_cmd(self, msg):
+        cmd = msg + "\r"
+        self.dev.write(cmd.encode())
+
+    def _read_line(self):
+        return self.dev.readline().decode()
+
+    def read_voltage(self, ch):
+        msg = ch+"R??"
+        self._send_cmd(msg)
+        reply = self._read_line()
+        voltage = reply
+
+        return voltage
+
+    def set_voltage(self, ch, voltage):
+        msg = ch+"V"+str(voltage)
+        self._send_cmd(msg)
---
+++ oxart/frontend/thorlabs_mdt693a_controller.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.5
+
+import argparse
+import sys
+
+from oxart.devices.thorlabs_mdt693a.driver import PiezoController
+from sipyco.pc_rpc import simple_server_loop
+from sipyco.common_args import simple_network_args, init_logger_from_args
+from oxart.tools import add_common_args
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="ARTIQ controller for the "
+        "Thorlabs MDT693A 3-channel open-loop piezo controller")
+    simple_network_args(parser, 9001)
+    parser.add_argument("-d", "--device", default=None, required=True,
+                        help="Device ip address")
+    add_common_args(parser)
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+    init_logger_from_args(args)
+
+    dev = PiezoController(args.device)
+
+    try:
+        simple_server_loop({"piezoController": dev}, args.bind, args.port)
+    except Exception:
+        dev.close()
+    else:
+        dev.close()
+    finally:
+        dev.save_setpoints()
+
+if __name__ == "__main__":
+    main()
