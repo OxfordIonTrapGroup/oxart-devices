b7954a5f7e6a84e6dece0a1b35d25d0cb789e606
David Nadlinger <code@klickverbot.at>
2021-05-18 21:38:53 +0100

    Add unit test example

---
+++ test/test_dummy.py
@@ -0,0 +1,11 @@
+"""
+Shows how to add unit tests that are automatically executed by the build
+intrastructure. Should be deleted as soon as there are some actual unit
+tests that can be used as an example.
+"""
+
+import unittest
+
+class DummyTest(unittest.TestCase):
+    def test_empty(self):
+        self.assertTrue(True)
