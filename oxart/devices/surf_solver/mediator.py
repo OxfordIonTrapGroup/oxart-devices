528e7c999085cd8eedbd3d28965226c74daa62f6
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2021-07-08 17:12:01 +0100

    surf_solver.mediator: Add keys 'f_axial' and 'f_rad_x' to change_dict
    This converts mode frequencies to field curvatures internally.

--- oxart/devices/surf_solver/mediator.py
+++ oxart/devices/surf_solver/mediator.py
@@ -291,6 +291,11 @@ class SURFMediator:
 
         new_wells = deepcopy(wave.fixed_wells[-1])
         for name, param_dict in change_dict.items():
+            if "f_rad_x" in param_dict:
+                param_dict["d2phidradial_h2"] = self.f_to_field(param_dict.pop("f_rad_x"))
+            if "f_axial" in param_dict:
+                param_dict["d2phidaxial2"] = self.f_to_field(param_dict.pop("f_axial"))
+
             idx = new_wells.name.index(name)
             for param, value in param_dict.items():
                 # exploit mutability of list
