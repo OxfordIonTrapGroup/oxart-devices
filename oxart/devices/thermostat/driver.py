32b769051abc5420ab7403f05b9d0c99e7feb6ab
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2023-11-09 09:27:15 +0000

    Update thermostat controller/driver
    Uses llama to push statistics of latest chunks to Grafana.

--- oxart/devices/thermostat/driver.py
+++ oxart/devices/thermostat/driver.py
@@ -4,6 +4,9 @@ https://git.m-labs.hk/M-Labs/thermostat/src/branch/master/pytec/pytec/client.py
 
 import socket
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class CommandError(Exception):
@@ -15,6 +18,15 @@ class Thermostat:
     def __init__(self, host, port=23, timeout=10):
         self._socket = socket.create_connection((host, port), timeout)
         self._lines = [""]
+        self._check_zero_limits()
+
+    def _check_zero_limits(self):
+        pwm_report = self.get_pwm()
+        for pwm_channel in pwm_report:
+            for limit in ["max_i_neg", "max_i_pos", "max_v"]:
+                if pwm_channel[limit]["value"] == 0.0:
+                    logger.warning("`{}` limit is set to zero on channel {}".format(
+                        limit, pwm_channel["channel"]))
 
     def _read_line(self):
         # read more lines
@@ -81,22 +93,16 @@ class Thermostat:
                   'ki': 0.02,
                   'kd': 0.0,
                   'output_min': 0.0,
-                  'output_max': 3.0,
-                  'integral_min': -100.0,
-                  'integral_max': 100.0},
-              'target': 37.0,
-              'integral': 38.41138597026372},
+                  'output_max': 3.0},
+              'target': 37.0},
              {'channel': 1,
               'parameters': {
                   'kp': 10.0,
                   'ki': 0.02,
                   'kd': 0.0,
                   'output_min': 0.0,
-                  'output_max': 3.0,
-                  'integral_min': -100.0,
-                  'integral_max': 100.0},
-              'target': 36.5,
-              'integral': nan}]
+                  'output_max': 3.0},
+              'target': 36.5}]
         """
         return self._get_conf("pid")
 
@@ -119,45 +125,14 @@ class Thermostat:
         return self._get_conf("postfilter")
 
     def report(self):
-        """Retrieve current status
-
-        Example::
-              [{'channel': 0,
-              'time': 76332.548,
-              'interval': 0.12,
-              'adc': 0.7265734615241562,
-              'sens': 8025.597054833033,
-              'temperature': 29.999977197542364,
-              'pid_engaged': True,
-              'i_set': 0.15560243296810894,
-              'vref': 1.5, 'dac_value': 1.5768012164840546,
-              'dac_feedback': 1.573,
-              'i_tec': 1.567,
-              'tec_i': 0.20000000000000018,
-              'tec_u_meas': 0.0680000000000005,
-              'pid_output': 0.14770870320992324},
-             {'channel': 0,
-              'time': 76332.548,
-              'interval': 0.12,
-              'adc': 0.7265734615241562,
-              'sens': 8025.597054833033,
-              'temperature': 29.999977197542364,
-              'pid_engaged': True,
-              'i_set': 0.15560243296810894,
-              'vref': 1.5, 'dac_value': 1.5768012164840546,
-              'dac_feedback': 1.573,
-              'i_tec': 1.567,
-              'tec_i': 0.20000000000000018,
-              'tec_u_meas': 0.0680000000000005,
-              'pid_output': 0.14770870320992324}]
-        """
+        """Retrieve current status"""
         return self._get_conf("report")
 
     def report_mode(self):
         """Start reporting measurement values
 
         Example of yielded data::
-            {'channel': 0,
+            [{'channel': 0,
              'time': 2302524,
              'adc': 0.6199188965423515,
              'sens': 6138.519310282602,
@@ -170,7 +145,9 @@ class Thermostat:
              'i_tec': 2.331,
              'tec_i': 2.0925,
              'tec_u_meas': 2.5340000000000003,
-             'pid_output': 2.067581958092247}
+             'pid_output': 2.067581958092247},
+            ...
+            ]
         """
         self._command("report mode", "on")
 
