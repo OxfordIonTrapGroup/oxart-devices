d35733c5085076081ab2a26ae529d04322238657
Mario Gely <mario.gely@physics.ox.ac.uk>
2023-11-20 11:26:29 +0000

    Brooks flow controller - asyncio bug

--- oxart/frontend/aqctl_brooks_4850.py
+++ oxart/frontend/aqctl_brooks_4850.py
@@ -2,6 +2,7 @@ import argparse
 
 from oxart.devices.brooks_4850.driver import Brooks4850
 from sipyco.pc_rpc import simple_server_loop
+import asyncio
 import sipyco.common_args as sca
 
 
@@ -9,7 +10,7 @@ def get_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d",
                         "--device",
-                        default="socket://10.255.6.178:9001",
+                        # default="socket://10.255.6.178:9001",
                         help="address (USB Serial Number or IP:port)")
     sca.simple_network_args(parser, 3255)
     sca.verbosity_args(parser)
@@ -23,7 +24,7 @@ def main():
 
     try:
         simple_server_loop({"Brooks4850": dev}, sca.bind_address_from_args(args),
-                           args.port)
+                           args.port, loop=asyncio.get_event_loop())
     finally:
         dev.close()
 
