a9f5381b5347ec0bb378cbabe3de70e021b825af
Mario Gely <mario.f.gely@gmail.com>
2023-11-20 12:33:53 +0000

    Updating the current_stabilizer driver with biquad feedback + a few asyncio bug corrections (#18)
    * current_stabilizer - adding biquad feedback (written by Marius/Ryan)
    
    * aqctl_thorlabs_tcube - (DPN) simple_server_loop settings
    
    * Brooks flow controller - asyncio bug
    
    * flake8
    
    * run yapf on everything
    
    * undo previous yapf change
    
    * flake8
    
    * run yapf on everything
    
    * using yapf v0.30
    
    ---------
    
    Co-authored-by: Mario Gely <mario.gely@physics.ox.ac.uk>

--- oxart/devices/stabilizer/current_stabilizer.py
+++ oxart/devices/stabilizer/current_stabilizer.py
@@ -56,6 +56,68 @@ class IIR:
         self.ba[3] = a1
         self.ba[4] = 0.
 
+    def configure_biquad(self, zeros, poles, gain=1.):
+        """Calulate biquad iir filter coeficents
+        The function constructs the iir coeficents for a transfer function with
+        desired zeros and poles.
+        :param zeros: list of upto two zero locations in Hz, must be real or
+            complex conjugate pairs.
+        :param poles: list of upto two pole locations in Hz, must be real or
+            complex conjugate pairs.
+        :param gain: gain scaling factor of transfer function.
+        """
+        def get_polynomial_coefs(factors):
+            "convert factors to coeficents"
+            if len(factors) == 0:
+                return [1., 0., 0.]
+            elif len(factors) == 1:
+                if factors[0] != 0.:
+                    return [1., 1. / factors[0], 0.]
+                else:
+                    return [0., 1., 0.]
+            elif len(factors) == 2:
+                div = factors[0] * factors[1]
+                if div == 0.:
+                    return [0., factors[0] + factors[1], 1.]
+                else:
+                    return [1., (factors[0] + factors[1]) / div, 1. / div]
+            else:
+                raise ValueError("Invalid number of factors")
+
+        def z_transform(s_coefs, t_update):
+            """
+            z-transformation of second order s polynomial in coefficients
+
+            This uses Tustin’s transformation
+            see https://arxiv.org/pdf/1508.06319.pdf
+
+            We drop a factor of 1/(1 + z^-1)^2 which is common to both
+            polynomials
+            """
+            c = 2 / t_update
+
+            z_coefs = [
+                s_coefs[0] + c * s_coefs[1] + c * c * s_coefs[2],
+                2 * s_coefs[0] - 2 * c * c * s_coefs[2],
+                s_coefs[0] - c * s_coefs[1] + c * c * s_coefs[2],
+            ]
+            return z_coefs
+
+        num_coefs = z_transform(get_polynomial_coefs([2 * np.pi * x for x in zeros]),
+                                self.t_update)
+        denom_coefs = z_transform(get_polynomial_coefs([2 * np.pi * x for x in poles]),
+                                  self.t_update)
+
+        # normalise to a0 = 1 & apply gain factor
+        num_coefs = [np.real(x / denom_coefs[0]) * gain for x in num_coefs]
+        denom_coefs = [np.real(x / denom_coefs[0]) for x in denom_coefs]
+
+        self.ba[0] = num_coefs[0]
+        self.ba[1] = num_coefs[1]
+        self.ba[2] = num_coefs[2]
+        self.ba[3] = -denom_coefs[1]
+        self.ba[4] = -denom_coefs[2]
+
     def set_x_offset(self, o):
         b = self.ba[:3].sum() * self.full_scale
         self.y_offset = b * o / self.I_set_range
@@ -190,6 +252,25 @@ class Stabilizer:
         assert self.channel in range(2)
         await set_feedback(self.fb_connection, self.channel, i, d, g)
 
+    async def set_feedback_biquad(self,
+                                  frontend_offset=250,
+                                  zeros=[],
+                                  poles=[],
+                                  gain=1.,
+                                  feedback_offset=0,
+                                  channel_offset=0):
+        d = CPU_DAC()
+        d.set_out(feedback_offset)
+        d.set_en(True)
+        i = IIR()
+        i.configure_biquad(zeros, poles, gain)
+        i.set_x_offset(channel_offset)
+        g = GPIO_HDR_SPI()
+        g.set_gpio_hdr(frontend_offset)
+
+        assert self.channel in range(2)
+        await set_feedback(self.fb_connection, self.channel, i, d, g)
+
     async def set_feedforward(self, coefficients=None, offset=0):
         if coefficients is None:
             coefficients = np.zeros(self.num_harmonics)
@@ -199,5 +280,4 @@ class Stabilizer:
         ff.set_cosine_amplitudes(cos_amps)
         ff.set_sine_amplitudes(sin_amps)
         ff.set_offset(offset)
-
         await set_feedforward(self.ff_connection, ff)
