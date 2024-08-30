eac15fa9dcd3c476bb87d26547bfe3585698ff2c
David Nadlinger <code@klickverbot.at>
2024-08-30 16:33:31 +0100

    logger_toptica_raman: Fix reconnection attempts
    The previous implementation had two (closely related) issues: It would
    not actually attempt to reconnect once an exception occurred, and also
    continue to log the old, out-of-date values.

--- oxart/frontend/logger_toptica_raman.py
+++ oxart/frontend/logger_toptica_raman.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 import asyncio
-from asyncio import wait_for
+import logging
 import time
 from influxdb import InfluxDBClient
-
 from toptica.lasersdk.asyncio.dlcpro.v2_0_1 import Client, NetworkConnection
+
+logger = logging.getLogger(__name__)
+
+# Maps user-facing parameter names (used for InflxuDB fields) to the DLC pro's internal
+# ones.
+parameters = {
+    "dl-current-act": "laser1:dl:cc:current-act",
+    "dl-voltage-act": "laser1:dl:cc:voltage-act",
+    "dl-temp-act": "laser1:dl:tc:temp-act",
+    "amp-current-act": "laser1:amp:cc:current-act",
+    "amp-voltage-act": "laser1:amp:cc:voltage-act",
+    "amp-temp-act": "laser1:amp:tc:temp-act",
+    "amp-seed-power": "laser1:amp:pd:seed:power",
+    "amp-power": "laser1:amp:pd:amp:power",
+    "shg-temp-act": "laser1:nlo:shg:tc:temp-act",
+    "shg-power": "laser1:nlo:pd:shg:power"
+}
 
 
 def get_argparser():
@@ -31,8 +47,8 @@ def get_argparser():
         "--timeout",
         default=60,
         type=int,
-        help="Time (seconds) between messages from device after which connection is " +
-        "considered faulty and program exits")
+        help="Timeout for controller responses before which connection " +
+        "is considered faulty and reconnection is attempted.")
     return parser
 
 
@@ -49,33 +65,25 @@ def main():
         try:
             influx_client.write_points([point])
         except ConnectionError:
-            print("ConnectionError: Influxdb down?")
+            logger.exception("ConnectionError: InfluxDB down?")
 
-    async def run():
-        # Dictionary containing location of parameters in dlc
-        parameters = {
-            "dl-current-act": "laser1:dl:cc:current-act",
-            "dl-voltage-act": "laser1:dl:cc:voltage-act",
-            "dl-temp-act": "laser1:dl:tc:temp-act",
-            "amp-current-act": "laser1:amp:cc:current-act",
-            "amp-voltage-act": "laser1:amp:cc:voltage-act",
-            "amp-temp-act": "laser1:amp:tc:temp-act",
-            "amp-seed-power": "laser1:amp:pd:seed:power",
-            "amp-power": "laser1:amp:pd:amp:power",
-            "shg-temp-act": "laser1:nlo:shg:tc:temp-act",
-            "shg-power": "laser1:nlo:pd:shg:power"
-        }
-        msg = {key: None for key in parameters.keys()}
+    async def run_poll_loop():
         async with Client(NetworkConnection(args.server)) as dlc:
             while True:
                 time.sleep(args.poll)
+                msg = {key: None for key in parameters.keys()}
                 for key in parameters.keys():
-                    try:
-                        msg[key] = await wait_for(dlc.get(parameters[key], float),
-                                                  timeout=args.timeout)
-                    except Exception:
-                        print('Error: Is DLC pro connected to network?')
+                    msg[key] = await asyncio.wait_for(dlc.get(parameters[key], float),
+                                                      timeout=args.timeout)
                 write_point(msg)
+
+    async def run():
+        while True:
+            try:
+                await run_poll_loop()
+            except Exception:
+                logger.exception("Is DLC pro connected to network?")
+                # Ignore exception and retry.
 
     loop.run_until_complete(run())
 
