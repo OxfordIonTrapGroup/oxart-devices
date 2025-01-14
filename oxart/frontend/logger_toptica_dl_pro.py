c67610b19adfa120f27f9739fe75fa097019e04d
ayushagrawal <ayush.agrawal6278@gmail.com>
2025-01-14 11:32:02 +0000

    Toptica DLPro Logger: Run formatter

--- oxart/frontend/logger_toptica_dl_pro.py
+++ oxart/frontend/logger_toptica_dl_pro.py
@@ -7,39 +7,39 @@ from influxdb import InfluxDBClient
 import importlib
 import logging
 
-
 logger = logging.getLogger(__name__)
 
 
 def get_argparser():
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument(
-        "-s", "--server", required=True, help="Laser controller address / hostname."
-    )
-    parser.add_argument(
-        "--firmware", default="v3_0_1", help="Firmware version of DLC Pro"
-    )
-    parser.add_argument(
-        "--name", required=True, help="Logical laser name, defines measurement name"
-    )
-    parser.add_argument(
-        "--influx-server", default="localhost", help="Influx server address"
-    )
-    parser.add_argument(
-        "--database", default="dl_pro_status", help="Influx database name."
-    )
-    parser.add_argument(
-        "--poll", default=30, type=int, help="Measurement polling period (seconds)"
-    )
+    parser.add_argument("-s",
+                        "--server",
+                        required=True,
+                        help="Laser controller address / hostname.")
+    parser.add_argument("--firmware",
+                        default="v3_0_1",
+                        help="Firmware version of DLC Pro")
+    parser.add_argument("--name",
+                        required=True,
+                        help="Logical laser name, defines measurement name")
+    parser.add_argument("--influx-server",
+                        default="localhost",
+                        help="Influx server address")
+    parser.add_argument("--database",
+                        default="dl_pro_status",
+                        help="Influx database name.")
+    parser.add_argument("--poll",
+                        default=30,
+                        type=int,
+                        help="Measurement polling period (seconds)")
     parser.add_argument(
         "--timeout",
         default=60,
         type=int,
-        help="Time (seconds) between messages from device after which connection is "
-        + "considered faulty and program exits",
+        help="Time (seconds) between messages from device after which connection is " +
+        "considered faulty and program exits",
     )
     return parser
 
@@ -49,13 +49,12 @@ def main():
 
     # Import corresponding DLC Pro module for the required firmware
     dlcpro_sdk = importlib.import_module(
-        ".lasersdk.asyncio.dlcpro.{}".format(args.firmware), "toptica"
-    )
+        ".lasersdk.asyncio.dlcpro.{}".format(args.firmware), "toptica")
 
     loop = asyncio.get_event_loop()
-    influx_client = InfluxDBClient(
-        host=args.influx_server, database=args.database, timeout=30
-    )
+    influx_client = InfluxDBClient(host=args.influx_server,
+                                   database=args.database,
+                                   timeout=30)
 
     def write_point(fields):
         point = {"measurement": args.name, "fields": fields}
@@ -80,21 +79,18 @@ def main():
         msg = {key: None for key in parameters.keys()}
         logger.info("Connecting to DLC Pro...")
         while True:
-            async with dlcpro_sdk.Client(
-                dlcpro_sdk.NetworkConnection(args.server)
-            ) as dlc:
+            async with dlcpro_sdk.Client(dlcpro_sdk.NetworkConnection(
+                    args.server)) as dlc:
                 logger.info("Established connection to DLC Pro")
                 while True:
                     time.sleep(args.poll)
                     for key in parameters.keys():
                         try:
-                            msg[key] = await wait_for(
-                                dlc.get(parameters[key], float), timeout=args.timeout
-                            )
+                            msg[key] = await wait_for(dlc.get(parameters[key], float),
+                                                      timeout=args.timeout)
                         except Exception as exception:
                             logger.error(
-                                f"Is DLC pro connected to network?: {exception}"
-                            )
+                                f"Is DLC pro connected to network?: {exception}")
                             break
                     else:
                         write_point(msg)
