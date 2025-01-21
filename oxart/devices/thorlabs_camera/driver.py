e50fc279ae86d9f70f158ce5d63e6841112f2925
Lorenzo Versini <lorenzo.versini@physics.ox.ac.uk>
2025-01-21 19:01:42 +0000

    formatted filies with black

--- oxart/devices/thorlabs_camera/driver.py
+++ oxart/devices/thorlabs_camera/driver.py
@@ -4,19 +4,21 @@ Most of functions are straight copied from the Thorlabs pylab library. Maybe thi
 
 from pylablib.devices import Thorlabs
 import logging
+
 logger = logging.getLogger(__name__)
+
 
 def list_serials():
     return Thorlabs.list_cameras_tlcam()
+
 
 class Camera(Thorlabs.ThorlabsTLCamera):
     def __init__(self, sn=None):
         super().__init__(serial=sn)
 
-
     def open(self):
         super().open()
-        logger.warning('Camera connected')
+        logger.warning("Camera connected")
         logger.warning(self.get_device_info())
         logger.warning(self.get_sensor_info())
 
@@ -29,30 +31,37 @@ class Camera(Thorlabs.ThorlabsTLCamera):
     def get_sensor_info_str(self):
         return repr(self.get_sensor_info())
 
-    def wait_for_frame(self, since='now', nframes=1, timeout=20.0, error_on_stopped=False):
-       super().wait_for_frame(since=since, nframes=nframes, timeout=timeout, error_on_stopped=error_on_stopped)
+    def wait_for_frame(
+        self, since="now", nframes=1, timeout=20.0, error_on_stopped=False
+    ):
+        super().wait_for_frame(
+            since=since,
+            nframes=nframes,
+            timeout=timeout,
+            error_on_stopped=error_on_stopped,
+        )
 
     def is_opened(self):
         """Verify the connection of the camera"""
         return super().is_opened()
-    
+
     def disconnect(self):
         self.stop_acquisition()
         super().close()
 
     def set_exposure_ms(self, exp: float):
         """Set exposure in seconds"""
-        super().set_exposure(exp/1000)
+        super().set_exposure(exp / 1000)
 
     def set_gain_dB(self, gain_dB, truncate=False):
-       super().set_gain(gain_dB, truncate)
-        
+        super().set_gain(gain_dB, truncate)
+
     def get_gain_dB(self):
         return super().get_gain()
 
     def get_gain_range_dB(self):
         return super().get_gain_range()
-    
+
     # ROI
     def set_roi(self, hstart=260, hend=800, vstart=0, vend=50):
         """Warning! This seems to be inconsistent up to some pixels in the vertical axis.
@@ -60,7 +69,9 @@ class Camera(Thorlabs.ThorlabsTLCamera):
         should be larger than the min value and smaller than the max (the size of the detector).
         Check with self.get_roi() to see the effective frame size.
         """
-        super().set_roi(hstart=hstart, hend=hend, vstart=vstart, vend=vend, hbin=1, vbin=1)
+        super().set_roi(
+            hstart=hstart, hend=hend, vstart=vstart, vend=vend, hbin=1, vbin=1
+        )
 
     def get_roi_lim(hbin=1, vbin=1):
         """Override parent class to output a dictionary. [LV] I believe sipyco does not allow some object types passing through the server.
@@ -71,25 +82,25 @@ class Camera(Thorlabs.ThorlabsTLCamera):
         """
         xlim, ylim = super().get_roi_limits(hbin, vbin)
         limits = {
-            'xlim': {
-                'min': xlim.min,
-                'max': xlim.max,
-                'pstep': xlim.pstep,
-                'sstep': xlim.sstep,
-                'maxbin': xlim.maxbin
+            "xlim": {
+                "min": xlim.min,
+                "max": xlim.max,
+                "pstep": xlim.pstep,
+                "sstep": xlim.sstep,
+                "maxbin": xlim.maxbin,
             },
-            'ylim': {
-                'min': ylim.min,
-                'max': ylim.max,
-                'pstep': ylim.pstep,
-                'sstep': ylim.sstep,
-                'maxbin': ylim.maxbin
-            }
+            "ylim": {
+                "min": ylim.min,
+                "max": ylim.max,
+                "pstep": ylim.pstep,
+                "sstep": ylim.sstep,
+                "maxbin": ylim.maxbin,
+            },
         }
         return limits
 
     def reset_roi_max(self):
         limits = self.get_roi_lim()
-        xlim_max = limits['xlim']['max']
-        ylim_max = limits['ylim']['max']
+        xlim_max = limits["xlim"]["max"]
+        ylim_max = limits["ylim"]["max"]
         self.set_roi(0, xlim_max, 0, ylim_max)
--- oxart/frontend/thorlabs_camera_controller.py
+++ oxart/frontend/thorlabs_camera_controller.py
@@ -1,19 +1,30 @@
 import argparse
 from sipyco.pc_rpc import simple_server_loop
-from sipyco.common_args import (simple_network_args, verbosity_args,
-                                init_logger_from_args)
+from sipyco.common_args import (
+    simple_network_args,
+    verbosity_args,
+    init_logger_from_args,
+)
 from oxart.devices.thorlabs_camera.driver import Camera, list_serials
+
 
 def get_argparser():
     parser = argparse.ArgumentParser()
     simple_network_args(parser, 4000)
     verbosity_args(parser)
-    parser.add_argument("--list", action="store_true",
-        help="list connected cameras (ignores all other arguments)")
-    parser.add_argument("--device", "-d", type=int, default=None,
-        help="camera serial number, uses first available if not supplied")
+    parser.add_argument(
+        "--list",
+        action="store_true",
+        help="list connected cameras (ignores all other arguments)",
+    )
+    parser.add_argument(
+        "--device",
+        "-d",
+        type=int,
+        default=None,
+        help="camera serial number, uses first available if not supplied",
+    )
     return parser
-
 
 
 def run_server(dev, args):
