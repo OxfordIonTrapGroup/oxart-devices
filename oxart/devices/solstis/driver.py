e5e62a505b05429bbffb8a6f43b85598bd80f477
ayushagrawal <ayush.agrawal6278@gmail.com>
2026-01-07 22:52:20 +0000

    yapfify

--- oxart/devices/solstis/driver.py
+++ oxart/devices/solstis/driver.py
@@ -9,6 +9,7 @@ logger = getLogger(__name__)
 
 
 class SolstisNotifier:
+
     def __init__(
         self,
         server,
@@ -27,9 +28,8 @@ class SolstisNotifier:
         """Connect to the Solstis websocket server and receive messages until
         connection drops."""
         logger.info("Connecting to Solstis at {}:{}".format(self.server, self.port))
-        async with connect(
-            "ws://{}:{}".format(self.server, self.port), ping_interval=None
-        ) as websocket:
+        async with connect("ws://{}:{}".format(self.server, self.port),
+                           ping_interval=None) as websocket:
             while True:
                 try:
                     raw_msg = await wait_for(websocket.recv(), timeout=self.timeout)
